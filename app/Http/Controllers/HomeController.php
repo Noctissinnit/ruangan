68c542265b2ea7c9304d2db3eff6e826da40a690
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2025-01-30 00:38:55 +0700

    fix: only today bookings

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Room;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -14,7 +15,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $rooms = Room::all();
+        $rooms = Room::with(["bookings" => function($query){
+            $query->where('bookings.date', Carbon::today());
+        }])->get();
         return view('home', compact('rooms'));
     }
 }
